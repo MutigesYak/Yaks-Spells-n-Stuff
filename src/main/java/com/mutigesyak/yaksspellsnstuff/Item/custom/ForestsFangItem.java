b67aeebcc502fba59db4e35bc551b4420ef66416
MuitgesYak <hirehg@gmx.net>
2025-06-25 21:05:05 +0200

    everything works

---
+++ src/main/java/com/mutigesyak/yaksspellsnstuff/Item/custom/ForestsFangItem.java
@@ -0,0 +1,9 @@
+package com.mutigesyak.yaksspellsnstuff.Item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class ForestsFangItem extends Item {
+    public ForestsFangItem(Properties properties) {
+        super(properties);
+    }
+}
