246b0a10b02e8e5f02fd08f6db9dd5d3fec396fd
MuitgesYak <hirehg@gmx.net>
2025-10-08 16:16:46 +0200

    Started making a weapon

---
+++ src/main/java/net/mutigesyak/yaksspellsnstuff/item/custom/ModToolTiers.java
@@ -0,0 +1,9 @@
+package net.mutigesyak.yaksspellsnstuff.item.custom;
+
+import net.minecraft.world.item.Tier;
+import net.mutigesyak.yaksspellsnstuff.util.ModTags;
+import net.neoforged.neoforge.common.SimpleTier;
+
+public class ModToolTiers {
+    public static final Tier DIVINE_SWORD = new SimpleTier(ModTags.Blocks)
+}
---
+++ src/main/java/net/mutigesyak/yaksspellsnstuff/util/ModTags.java
@@ -0,0 +1,8 @@
+package net.mutigesyak.yaksspellsnstuff.util;
+
+import com.jcraft.jorbis.Block;
+import net.minecraft.tags.TagKey;
+
+public class ModTags {
+    //public static final TagKey<Block>
+}
